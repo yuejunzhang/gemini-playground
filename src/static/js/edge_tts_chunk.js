30fd30a92b5eeefc2ab264db93e2bc00f9e90898
yuejunzhang <58799137+yuejunzhang@users.noreply.github.com>
2025-01-25 21:41:09 +0800

    2

--- src/static/js/edge_tts_chunk.js
+++ src/static/js/edge_tts_chunk.js
@@ -250,7 +250,7 @@ stopPlay=true;
     return result;
   }
 }
-export async function playChunk(text,voiceindex=0,rate = 0,pitch=0,autpPlay=true) {
+async function playChunk(text,voiceindex=0,rate = 0,pitch=0,autpPlay=true) {
   let data;
   stopPlay=false;
     // 设置连接超时
@@ -278,7 +278,7 @@ export async function playChunk(text,voiceindex=0,rate = 0,pitch=0,autpPlay=true
     audioElement.src = data;
     audioElement.play();
 }
-export function stopPlayChunk(){
+function stopPlayChunk(){
   stopPlay=true;
   if(!audioElement.paused ){audioElement.currentTime = audioElement.duration;}
   audioElement.pause();
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -5,7 +5,7 @@ import { CONFIG } from './config/config.js';
 import { Logger } from './utils/logger.js';
 import { VideoManager } from './video/video-manager.js';
 import { ScreenRecorder } from './video/screen-recorder.js';
-import * as edgeTTSChunk from './edge_tts_chunk.js';
+// import * as edgeTTSChunk from './edge_tts_chunk.js';
 
 /**
  * @fileoverview Main entry point for the application.
@@ -108,7 +108,7 @@ function logMessage(message, type = 'system') {
             emoji.textContent = '⚙️';
             console.log(message)
             if(message.includes("Turn complete")){
-                edgeTTSChunk.stopPlayChunk();
+                stopPlayChunk();
                 const msgDiv = document.createElement('div');
                 msgDiv.classList.add('msg-div');
                 // msgDiv.id = `msg-${msglist.children.length + 1}`;
@@ -122,7 +122,7 @@ function logMessage(message, type = 'system') {
             emoji.textContent = '🤖';
             if (msglist.lastElementChild) {
                 msglist.lastElementChild.textContent += message;
-                edgeTTSChunk.playChunk(message,2,0,0,true);
+                playChunk(message,2,0,0,true);
                 msglist.scrollTop = msglist.scrollHeight;
             }
             break;
