f14ebc4ee2fa3dac3dc94613e99c8fb5d291a7f1
yuejunzhang <58799137+yuejunzhang@users.noreply.github.com>
2025-01-25 20:47:10 +0800

    1

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -107,7 +107,7 @@ function logMessage(message, type = 'system') {
             emoji.textContent = '⚙️';
             console.log(message)
             if(message.includes("Turn complete")){
-                const msgDiv = document.createElement('div');
+                const msgDiv = document.createElement('pre');
                 msgDiv.classList.add('msg-div');
                 // msgDiv.id = `msg-${msglist.children.length + 1}`;
                 msglist.appendChild(msgDiv);
