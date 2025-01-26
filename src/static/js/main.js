58f5b7943e4a84ca2a60742c3f44ef6dd0b081ab
yuejunzhang <58799137+yuejunzhang@users.noreply.github.com>
2025-01-26 10:02:10 +0800

    1

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -132,9 +132,9 @@ async function logMessage(message, type = 'system') {
                 // await playChunk(message,2,0,0,false);
 //                 // 发送自定义事件
                 chunk += message;
-                // console.log(message);
+                console.log(message);
                 if (/[:：?？.。！!；;]$/u.test(message)) {
-                    console.log(message);
+                    // console.log(message);
                     chunk=chunk.replace(/[\\*#]/g, '');
                     // await playChunk(message,2,0,0,false);
                     // 发送自定义事件
