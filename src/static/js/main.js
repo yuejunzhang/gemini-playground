cb3073f8e15d8d75754ef460b547f51fdff9a16c
yuejunzhang <58799137+yuejunzhang@users.noreply.github.com>
2025-02-28 22:12:49 +0800

    1

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -555,7 +555,7 @@ async function handleVideoToggle() {
             await videoManager.start(fpsInput.value,(frameData) => {
                 if (isConnected ) {
                     client.sendRealtimeInput([frameData]);
-                    console.log("send frameData");
+                    logMessage('send frameData', 'system');
                 }
             });
 
