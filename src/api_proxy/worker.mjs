24694f4d6571b8f259da67027ad5d15e9ea4e143
yuejunzhang <58799137+yuejunzhang@users.noreply.github.com>
2025-06-02 14:19:52 +0800

    v1alpha

--- src/api_proxy/worker.mjs
+++ src/api_proxy/worker.mjs
@@ -69,7 +69,7 @@ const handleOPTIONS = async () => {
 };
 
 const BASE_URL = "https://generativelanguage.googleapis.com";
-const API_VERSION = "v1beta";
+const API_VERSION = "v1alpha";
 
 // https://github.com/google-gemini/generative-ai-js/blob/cf223ff4a1ee5a2d944c53cddb8976136382bee6/src/requests/request.ts#L71
 const API_CLIENT = "genai-js/0.21.0"; // npm view @google/generative-ai version
